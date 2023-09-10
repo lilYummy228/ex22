d2f79b85b2bd0748c83346bbf9bc4368d259dd6e
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-09-10 19:11:57 +0500

    123

--- ex22/Program.cs
+++ ex22/Program.cs
@@ -10,7 +10,41 @@ namespace ex22
     {
         static void Main(string[] args)
         {
+            int[] array = new int[30];
+            Random random = new Random();
+            int minRandomValue = 0;
+            int maxRandomValue = 10;
+            int repeatitionCount = 0;
+            int repeatitions = 1;
+            int repeatedNumber = 0;
 
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(minRandomValue, maxRandomValue);
+                Console.Write($"{array[i]}");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] == array[i - 1])
+                {
+                    repeatitions++;
+                }
+                else
+                {
+                    repeatitions = 1;
+                }
+
+                if (repeatitions > 1)
+                {
+                    repeatitionCount = repeatitions;
+                    repeatedNumber = array[i];
+                }
+            }
+
+            Console.WriteLine($"Повторений: {repeatitionCount}\nЧисло: {repeatedNumber}");
         }
     }
 }
